8b493d052f0235b612b61b422df8c295a4fa0644
Adrien <adrien.lachambre@bonitasoft.com>
2020-03-05 17:15:32 +0100

    reset

---
+++ src-connectors/org/mycompany/connector/AbstractCustomDefinitionImpl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.mycompany.connector;
+
+import org.bonitasoft.engine.connector.AbstractConnector;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+/**
+ * This abstract class is generated and should not be modified.
+ */
+public abstract class AbstractCustomDefinitionImpl extends AbstractConnector {
+
+	@Override
+	public void validateInputParameters() throws ConnectorValidationException {
+
+	}
+
+}
---
+++ src-filters/org/mycompany/connector/AbstractCustomActorDefinitionImpl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.mycompany.connector;
+
+import org.bonitasoft.engine.filter.AbstractUserFilter;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+/**
+ * This abstract class is generated and should not be modified.
+ */
+public abstract class AbstractCustomActorDefinitionImpl extends AbstractUserFilter {
+
+	@Override
+	public void validateInputParameters() throws ConnectorValidationException {
+
+	}
+
+}
